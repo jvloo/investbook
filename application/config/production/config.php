0c786105bc0bd61d0b006c5d75626b2d582fd267
Xavier Loo <xavier@senangprint.com>
2020-11-30 13:55:32 +0800

    Update

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -1,7 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['base_url']   = 'http://dev.senangprint.com/investbook';
+// $config['base_url']   = 'http://dev.senangprint.com/investbook';
+$config['base_url']   = 'http://investbook.herokuapp.com/';
 $config['index_page'] = '';
 
 $config['api_url'] = 'http://dev.senangprint.com/investbook/api/';
