f0515ae9121b699f17e1d52c2d346abd579a5d31
Xavier Loo <xavier@senangprint.com>
2020-12-01 00:58:45 +0800

    Update

--- application/config/development/config.php
+++ application/config/development/config.php
@@ -2,6 +2,6 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['api_url']  = 'http://localhost/investbook-api/';
-$config['app_url']  = 'http://localhost/investbook/';
+$config['app_url']  = 'http://localhost/investbook-v1/';
 
 $config['base_url'] = $config['app_url'];
